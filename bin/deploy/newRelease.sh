ad49bcad12e5a69c05bb5982e9b7f0d72364c882
Gilles Hoarau <unicolored@gmail.com>
2025-03-17 20:23:57 +0100

    Update release script to use Yarn for package management
    Replaced `npm install` with Yarn commands to ensure consistency with dependency management and lockfile updates. This aligns with the existing use of Yarn in the project and simplifies the release workflow.

--- bin/deploy/newRelease.sh
+++ bin/deploy/newRelease.sh
@@ -28,8 +28,7 @@ git add --all && git commit -m "Preparing new release ${NEW_VERSION}..." || true
 NEXT_RELEASE="${NEW_VERSION}" # Bump manually (ex. 18.2.0)
 git flow release start "${NEXT_RELEASE}"
 sed -i '' "s/\"version\": \".*\"/\"version\": \"${NEXT_RELEASE}\"/g" package.json
-#yarn set version stable && yarn install && npx update-browserslist-db@latest
-npm install
+yarn set version stable && yarn install && npx update-browserslist-db@latest
 
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 echo "✋ $PACKAGE_VERSION"
