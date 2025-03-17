c39b1150c11ced353f2701b743b39d5f48c7c7d5
Gilles Hoarau <unicolored@gmail.com>
2025-03-17 17:52:26 +0100

    Set up gain control for sound playback
    Added a gain node to manage audio volume, allowing finer control over output levels. Connected the oscillator through the gain node to the audio context destination and set the gain to an initial value of 0.033.

--- src/initial-sh.ts
+++ src/initial-sh.ts
@@ -110,10 +110,20 @@ export class InitialSh extends LitElement {
 
   private playSound(frequency: number, type: OscillatorType, duration: number) {
     if (!this.audioContext) return;
+
+    const gainNode = this.audioContext.createGain();
+
     const osc = this.audioContext.createOscillator();
     osc.type = type;
     osc.frequency.value = frequency;
-    osc.connect(this.audioContext.destination);
+
+    // Connect the oscillator to the gain node, then to the output
+    osc.connect(gainNode);
+    gainNode.connect(this.audioContext.destination);
+
+    // Set initial volume (0.5 = 50% volume)
+    gainNode.gain.setValueAtTime(0.033, this.audioContext.currentTime);
+
     osc.start();
     osc.stop(this.audioContext.currentTime + duration);
   }
